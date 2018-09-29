d1f8dd60a7980f2fd6e0553bb35e268683bf1ab1
ntchirkova <nina.tchirkova@students.olin.edu>
2018-09-29 17:31:28 -0400

    skeleton mostly complete

--- robot_localizer/scripts/robot_localizer.py
+++ robot_localizer/scripts/robot_localizer.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from visualization_msgs.msg import Marker
 import statistics
 import time, numpy, math, rospy
-import occupancy_field
+import occupancy_field as ocf
 
 class RobotLocalizer(object):
     """
@@ -26,10 +26,13 @@ class RobotLocalizer(object):
         # store how it's moved ie.
         self.xs = None
         self.ys = None
-        self.field = occupancy_field.OccupancyField()
+        self.field = ocf.OccupancyField()
+        self.particles = [] #list of particles, will be updated later
 
 
     def process_scan(self, m):
+        """Storing lidar data
+        """
         ranges = m.ranges
         xs = []
         ys = []
@@ -47,11 +50,54 @@ class RobotLocalizer(object):
         self.xs = xs
         self.ys = ys
 
-    def generate_random_points(self):
+    def gen_init_particles(self):
+        """Generating random particles with x, y, and t values"""
+        #TODO: test width and height, generate random particles
         width = field.map.info.width
         height = field.map.info.height
         print(width)
         print(height)
+        #this does not work yet but is a a start this needs location and orientation
+
+    def gen_neighbor_particles(self):
+        """Generates particles around given points"""
+        #TODO:
+        pass
+
+    def find_all_nearest_objects(self):
+        """Determines nearest non-zero point of all point (loops through)"""
+        #TODO:
+        pass
+
+    def compare_points(self):
+        """Compares particles to lidar scans, returns weights / probablility values"""
+        #TODO:
+        pass
+
+    def teleop(self):
+        """Adds teleop functionality, records encoder values"""
+        #TODO:
+        pass
+
+    def get_encoder_value(self):
+        """Records odom movement, translate to x, y, and theta"""
+        #TODO:
+        pass
+
+
+
+    """
+    Functions to write or figure out where they are:
+    - odom to map of robot pose
+    - robot pose nearest object
+    - particles nearest object
+    - choose particles that might be robot - viable particles
+    - generate particles around viable particles
+    - move robot certain distance then stop (maybe just teleop)
+    - get encoder values of robot
+    - transform particles by encoder values
+    -
+    """
 
 
     def run(self):
